a5608c26d162c1ee2bcef03892f9b0e093130b3d
Joatham Pérez Expósito <jpe.gsc@gmail.com>
2021-06-23 10:48:08 +0100

    Se añaden comentarios a los test Creación del proyecto test-unitarios #1

--- test-unitarios/test_mymodule.py
+++ test-unitarios/test_mymodule.py
@@ -8,10 +8,10 @@ import mymodule
 class TestMyModule(unittest.TestCase):
     
     def test_sum(self):
-        self.assertEqual(mymodule.sum(5, 7), 12)
+        self.assertEqual(mymodule.sum(5, 7), 12, "No se ha obtenido el resultado esperado")
 
     def test_error_sum(self):
-        self.assertNotEqual(mymodule.sum(5, 6), 12)
+        self.assertNotEqual(mymodule.sum(5, 6), 12, "El resultado obtenido debe de ser diferente 11 <> 12")
 
 
 if __name__ == "__main__":
